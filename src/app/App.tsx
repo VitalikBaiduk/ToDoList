66274399594d88491a2890f70ab299dc7657d220
VitalikBaiduk <vitalilbaiduj@icloud.com>
2022-01-24 12:14:03 +0300

    delete component 404 error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -67,8 +67,8 @@ function App({demo = false}: PropsType) {
                 <Routes>
                     <Route path="/" element={<TodolistsList demo={demo}/>}/>
                     <Route path="login" element={<Login/>}/>
-                    <Route path="404" element={<h1 style={{textAlign: "center"}}>404: PAGE NOT FOUND</h1>}/>
-                    <Route path={"*"} element={<Navigate to={"404"}/>}/>
+                    {/*<Route path="404" element={<h1 style={{textAlign: "center"}}>404: PAGE NOT FOUND</h1>}/>*/}
+                    <Route path={"*"} element={<Navigate to={"login"}/>}/>
                 </Routes>
             </Container>
         </div>
